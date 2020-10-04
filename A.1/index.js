b34c5c962f9a9ead731a9792e350158ad453e2c3
nguyenvanliem0501 <nguyenvanliem0501@gmail.com>
2020-10-04 12:12:52 +0700

    first commit

---
+++ A.1/index.js
@@ -0,0 +1,18 @@
+console.log("Tìm số đối diện với số mà bạn nhập vào trên vòng tròn:");
+while(true){
+    var n = parseInt(prompt("Enter a number: "));
+    if(n%2 == 0 && n<4 && n>20){
+    var inputNumber = parseInt(prompt("Enter a inputNumber"));
+    }
+}
+var i = inputNumber + (n/2);
+    alert(`Số đối diện với ${inputNumber} la ${i}`);
+
+// function findOppositeNumber( n,  inputNumber){
+//     for (let index = 0; index < n; index++) {
+//         if(n%2==0 && 4<=n<=20){
+            
+//         }
+        
+//     }
+// }
---
+++ A.2/index.js
@@ -0,0 +1,4 @@
+console.log("Viết hàm nhận vào 2 chuỗi s1 và s2, trả về đầu ra là một chuỗi mới chứa các kí tự xe kẽ cùng vị trí vị trí của 2 chuỗi với nhau");
+let str1 = ("abc", "123");
+let str2 = ("abc" , "0123");
+let str3 = ("abcd" , "123");
